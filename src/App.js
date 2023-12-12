2e89925486c6ab9bb4b421ed2eefb58cf4b833d8
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-12 22:24:29 +0200

    add randomNumber to arr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [arr, setArr] = useState([4, 7, 3, 8]);
+
+
+  function push() {
+    let randomNumber = (Math.floor(Math.random()*1000) + 1);
+    setArr([...arr, randomNumber])
+  }
+
+
   return (
     <>
-      
+      <h1>{arr}</h1>
+      <button onClick={push}>Push</button>
+      <ul>
+        {arr.map((item, idx) => <li key={idx}>{item}</li>)}
+      </ul>
     </>
   );
 }
